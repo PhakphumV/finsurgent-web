25f7afe2e244fbb506e9962dde7d973a37991627
Phakphum Visetnut <phakphum.visetnut@gmail.com>
2023-01-03 21:23:22 +0700

    Use env var

--- stock.php
+++ stock.php
@@ -8,7 +8,27 @@
 <body>
   <!-- HEADER -->
   <?php render_header("STOCK"); ?>
+
+  <?php
+  // Connecting to DB
+
+  $db_host = $_ENV['FSG_DB_HOST'];
+  echo $db_host;
+  $db_username = $_ENV['FSG_DB_USER'];
+  $db_password = $_ENV['FSG_DB_PASS'];
+  $db_name = $_ENV['FSG_DB_NAME'];
+
+  // $db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+
+  // if (!$db_conn) {}
+  // $query = "SELECT * FROM u549428984_python.mkd_data_current";
+  // $result = mysqli_query($conn, $query);
+
+  // mysqli_close($db_conn);
+  ?>
+
   <?php render_footer("STOCK"); ?>
+  <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
 </body>
 
 </html>
