e636c10d9cc1488b409caac13b1b9b228e2eff2f
Phakphum Visetnut <phakphum.visetnut@gmail.com>
2023-01-03 21:32:14 +0700

    Try connect db

--- stock.php
+++ stock.php
@@ -13,18 +13,22 @@
   // Connecting to DB
 
   $db_host = getenv('FSG_DB_HOST');
+  $db_username = getenv('FSG_DB_USER');
+  $db_password = getenv('FSG_DB_PASS');
+  $db_name = getenv('FSG_DB_NAME');
+
   echo "DB Host : ".$db_host;
-  // $db_username = $_ENV['FSG_DB_USER'];
-  // $db_password = $_ENV['FSG_DB_PASS'];
-  // $db_name = $_ENV['FSG_DB_NAME'];
+  echo "DB Name : ".$db_name;
 
-  // $db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+  $db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
-  // if (!$db_conn) {}
+  if (!$db_conn) {
+    echo "Cannon connect db";
+  }
   // $query = "SELECT * FROM u549428984_python.mkd_data_current";
   // $result = mysqli_query($conn, $query);
 
-  // mysqli_close($db_conn);
+  mysqli_close($db_conn);
   ?>
 
   <?php render_footer("STOCK"); ?>
