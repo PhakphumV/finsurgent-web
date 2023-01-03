aafeae329fbfad0b4039288e98a56595e597aa62
Phakphum Visetnut <phakphum.visetnut@gmail.com>
2023-01-03 21:36:26 +0700

    Try render symbols

--- stock.php
+++ stock.php
@@ -9,28 +9,44 @@
   <!-- HEADER -->
   <?php render_header("STOCK"); ?>
 
-  <?php
-  // Connecting to DB
+  <div class="container text-center">
+    <div class="row row-cols-5">
+      <?php
+      // Connecting to DB
 
-  $db_host = getenv('FSG_DB_HOST');
-  $db_username = getenv('FSG_DB_USER');
-  $db_password = getenv('FSG_DB_PASS');
-  $db_name = getenv('FSG_DB_NAME');
+      $db_host = getenv('FSG_DB_HOST');
+      $db_username = getenv('FSG_DB_USER');
+      $db_password = getenv('FSG_DB_PASS');
+      $db_name = getenv('FSG_DB_NAME');
 
-  echo "DB Host : ".$db_host;
-  echo "DB Name : ".$db_name;
+      $db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 
-  $db_conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
+      if (!$db_conn) {
+        echo "Cannon connect db";
+      }
+      $query = "SELECT * FROM u549428984_python.mkd_data_current";
+      $result = mysqli_query($conn, $query);
 
-  if (!$db_conn) {
-    echo "Cannon connect db";
-  }
-  // $query = "SELECT * FROM u549428984_python.mkd_data_current";
-  // $result = mysqli_query($conn, $query);
+      if (mysqli_num_rows($result) > 0) {
+      ?>
 
-  mysqli_close($db_conn);
-  ?>
+        <?php
+        while ($row = mysqli_fetch_assoc($result)) {
+          // echo "id: " . $row["id"] . " - Name: " . $row["name"] . " - Email: " . $row["email"] . "<br>";
+        ?>
+          <div class="col">
+            <?php echo $row['symbol']; ?>
+          </div>
+      <?php
+        }
+      } else {
+        echo "No data found in the database.";
+      }
 
+      mysqli_close($db_conn);
+      ?>
+    </div>
+  </div>
   <?php render_footer("STOCK"); ?>
   <script src="https://unpkg.com/isotope-layout@3/dist/isotope.pkgd.min.js"></script>
 </body>
