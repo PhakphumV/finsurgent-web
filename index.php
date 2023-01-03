53a6639d03b7f599196cbf2bd0deee6ca672033f
Phakphum Visetnut <phakphum.visetnut@gmail.com>
2023-01-03 20:47:15 +0700

    rename _footer to _layout, add render_head_tag

--- index.php
+++ index.php
@@ -1,36 +1,9 @@
-<?php require_once("_footer.php"); ?>
+<?php require_once("_layout.php"); ?>
 
 <!DOCTYPE html>
-<html class="no-js" lang="">
+<html class="no-js" lang="th">
 
-<head>
-  <meta charset="utf-8" />
-  <title></title>
-  <meta name="description" content="" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-  <meta property="og:title" content="" />
-  <meta property="og:type" content="" />
-  <meta property="og:url" content="" />
-  <meta property="og:image" content="" />
-
-  <link rel="manifest" href="site.webmanifest" />
-  <link rel="apple-touch-icon" href="icon.png" />
-  <!-- Place favicon.ico in the root directory -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
-  <style>
-    @import url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-  </style>
-  <link rel="stylesheet" href="css/normalize.css" />
-  <link rel="stylesheet" href="css/main.css" />
-  <link rel="stylesheet" href="css/finsurgent.css" />
-
-
-  <meta name="theme-color" content="#fafafa" />
-</head>
+<?php render_head_tag(); ?>
 
 <body>
   <!-- HEADER -->
