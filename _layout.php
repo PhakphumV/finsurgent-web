8ece3a429758d40029c969998a9c75b3e79c623d
Phakphum Visetnut <phakphum.visetnut@gmail.com>
2023-01-04 22:29:32 +0700

    Add jquery

--- _layout.php
+++ _layout.php
@@ -119,6 +119,7 @@ function render_footer($current_page = "Home")
       <p class="text-center text-muted">&copy; 2023 Finsurgent</p>
     </footer>
   </div>
+  <script src="https://code.jquery.com/jquery-3.6.3.slim.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   <script src="js/vendor/modernizr-3.11.2.min.js"></script>
   <script src="js/plugins.js"></script>
